f462042df5aa153175a9a00ebd750744e3866b28
宫新程 <gongxincheng@jsh.com>
2018-09-11 15:06:17 +0800

    style

--- microservice-java1234/microservice/microservice-student-consumer-80/src/main/java/com/java1234/StudentConsumerApplication_80.java
+++ microservice-java1234/microservice/microservice-student-consumer-80/src/main/java/com/java1234/StudentConsumerApplication_80.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(
+    exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class StudentConsumerApplication_80 {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentConsumerApplication_80.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StudentConsumerApplication_80.class, args);
+  }
 }
