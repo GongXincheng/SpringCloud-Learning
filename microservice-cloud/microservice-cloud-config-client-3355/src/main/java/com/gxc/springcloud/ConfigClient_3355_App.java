87a53395e186be6a1065bb9e4532b8b5ac6da5ef
宫新程 <gongxincheng@jsh.com>
2018-09-13 15:10:38 +0800

    SpringCloud Config Client

---
+++ microservice-cloud/microservice-cloud-config-client-3355/src/main/java/com/gxc/springcloud/ConfigClient_3355_App.java
@@ -0,0 +1,16 @@
+package com.gxc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 宫新程
+ * @date 2018/9/13 15:05
+ */
+@SpringBootApplication
+public class ConfigClient_3355_App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClient_3355_App.class, args);
+  }
+}
